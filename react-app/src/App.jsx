90ec6ac3b1075cac29d34cd42de0b8a14670666c
Tej <jupitersorbeet@gmail.com>
2025-01-10 17:26:07 +0530

    assignment completed

---
+++ react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Tejas Philip Thomas</h1>
+      <h2>contact : jupitersorbeet@gmail.com</h2>
+    </div>
+  );
+}
+export default App;
